73f649d48257eed33eabc849abd1c7d16db71642
Siracenco Serghei <108734530+SiracencoSerghei@users.noreply.github.com>
2024-02-12 11:14:59 +0100

    finished last extra query

--- sql/query_12.sql
+++ sql/query_12.sql
@@ -1,21 +1,73 @@
+-- Оцінки студентів у певній групі з певного предмета на всіх заняттях.
+
+
+WITH RankedGrades AS (
+  SELECT
+    gr.name AS "group",
+    d.subject_name AS subject,
+    s.name AS student,
+    g.value AS value,
+    g.date AS  date,
+    ROW_NUMBER() 
+    OVER (
+        PARTITION BY d.subject_name, gr.name, s.name 
+        ORDER BY g.date DESC
+        ) AS rnk
+  FROM
+    grades g
+    LEFT JOIN students s ON s.id = g.student_id
+    LEFT JOIN subjects d ON g.subject_id = d.id
+    LEFT JOIN groups gr ON s.group_id = gr.id
+  WHERE
+    gr.id = 3 AND d.id = 3
+)
+SELECT
+  "group",
+  subject,
+  student,
+  value,
+  date,
+  rnk
+FROM
+  RankedGrades
+ 
+
+-- ======================================================================
+
 -- Оцінки студентів у певній групі з певного предмета на останньому занятті.
 
-SELECT gr.name AS "group", 
-	s.name as student, 
-	d.subject_name AS subject, 
-	t.teacher_name AS teacher, 
-	value, 
-	date
-FROM grades g
-LEFT JOIN students s ON s.id = g.student_id 
-LEFT JOIN subjects d ON g.subject_id = d.id 
-LEFT JOIN teachers t ON d.teacher_id = t.id 
-LEFT JOIN groups gr ON s.group_id = gr.id 
-WHERE gr.id = 3 AND d.id = 3  
-AND date = (
-	SELECT MAX(date)
-	FROM grades g
-	LEFT JOIN students s ON s.id = g.student_id 
-	WHERE s.group_id = 3 AND g.subject_id = 1
+WITH RankedGrades AS (
+  SELECT
+    gr.name AS "group",
+    d.subject_name AS subject,
+    s.name AS student,
+    g.value AS value,
+    g.date AS  date,
+    ROW_NUMBER() 
+    OVER (
+        PARTITION BY d.subject_name, gr.name, s.name 
+        ORDER BY g.date DESC
+        ) AS rnk
+  FROM
+    grades g
+    LEFT JOIN students s ON s.id = g.student_id
+    LEFT JOIN subjects d ON g.subject_id = d.id
+    LEFT JOIN groups gr ON s.group_id = gr.id
+  WHERE
+    gr.id = 3 AND d.id = 3
 )
-ORDER BY value DESC;
+
+
+
+SELECT
+  "group",
+  subject,
+  student,
+  value,
+  date
+FROM
+  RankedGrades
+WHERE
+  rnk = 1
+ORDER BY
+  value DESC;
